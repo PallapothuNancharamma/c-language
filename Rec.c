9990f2a36faae34e5c6c5840844b269b21add87f
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2024-09-30 19:06:32 +0530

    Create Rec.c

---
+++ Rec.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int l,b,p;
+    printf("Enter the length and bredth:");
+    scanf("%d%d",&l,&b);
+    p=2*(l+b);
+    printf("The perimeter of rectangle is %d",p);
+}
