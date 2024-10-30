272a805d0d64aed1d0c042625c8a948a04fd59dd
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2024-10-30 15:50:19 +0530

    Create evenodd switch.c

---
+++ evenodd switch.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main();
+{
+    int n;
+    float c;
+    printf("enter the value of n ");
+    scanf("%d",&n);
+     c=n%2;
+    switch(c)
+    {
+     case 0:printf("%d is a even number",n);
+     break;
+     default:printf("%d is a odd number",n);
+     break;
+    }
+}
