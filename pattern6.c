e3985879c66d362bc90463fa98c0bd4e02d51438
PallapothuNancharamma <pallapothunancharamma@gmail.com>
2025-01-03 11:55:58 +0530

    pattern6.c

---
+++ pattern6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    char n='A';
+    for(i=1;i<=6;i++)
+    { 
+        for(j=1;j<=i;j++)
+        {
+            
+            printf("%c\t",n++);
+        }
+        printf("\n");
+    }
+}
